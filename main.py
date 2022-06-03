479c0cb8018de159a8d6b775728aa68626a6a841
HonzaP <jan.podavka@tul.cz>
2022-06-03 22:20:43 +0200

    graph init ....now add make graph from file

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import sys
+
+
+class Graph:
+    def __init__(self, vertex):
+        self.V = vertex
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+
 if __name__ == '__main__':
-    print("Init project")
+    n = None
+    for line in sys.stdin:
+        if n is None:
+            n = line
+        else:
+            print(line)
