8fbb455ffc41ab94c1e67a79a513a05367733701
HonzaP <jan.podavka@tul.cz>
2022-06-04 00:21:01 +0200

    #20 edge on line and #20 edge on column - done 4-neigborhood

--- main.py
+++ main.py
@@ -6,11 +6,19 @@ class Graph:
         self.V = [[0] * size] * size
         self.graph = []
 
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
+    def add_edge(self, x, y, w):
+        self.graph.append([x, y, w])
 
-    def create_neighborhood(self):
-        pass
+    def create_4_neighborhood(self, size):
+        for i in range(0, size):
+            for j in range(0, size):
+                print(i, j)
+                if i + 1 < size:
+                    self.add_edge((i, j), (i + 1, j), abs(self.V[i][j] - self.V[i + 1][j]))
+                if j + 1 < size:
+                    self.add_edge((i, j), (i, j + 1), abs(self.V[i][j] - self.V[i][j+1]))
+            print(len(g.graph))
+
 
 def parse_input(input_lines, size):
     graph = Graph(size)
@@ -20,11 +28,9 @@ def parse_input(input_lines, size):
     return graph
 
 
-
-
 if __name__ == '__main__':
     n = int(sys.stdin.readline())
     lines = sys.stdin.readlines()
     g = parse_input(lines, n)
-
-
+    g.create_4_neighborhood(n)
+    print(g.graph)
