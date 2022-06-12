3e98a48bb262c3731b5944387b6e7852362e47ad
Anastazie Rosova <anastazie.rosova@tul.cz>
2022-06-12 20:24:10 +0200

    Krus

--- Krus.py
+++ Krus.py
@@ -1,32 +1,31 @@
-
 from itertools import chain
 import numpy as np
+import sys
+
+sys.setrecursionlimit(3000)
 
 
 class Graph:
     def __init__(self, image, size):
         self.size = size
         self.values = list(chain.from_iterable(image))
-        self.V, self.E = self.getVerticles(image, size)
-
-        self.parent = {x: x for x in self.V}
-        self.rank = {x: 0 for x in self.V}
-        self.max_values = {x: self.values[idx] for idx, x in enumerate(self.V)}
+        self.parent, self.E = self.getVerticles(image, size)
+        self.max_values = {x: self.values[idx] for idx, x in enumerate(self.parent.values())}
 
     def getVerticles(self, values, size):
-        V = []
+        V = {}
         E = []
         for i in range(size):
             for j in range(size):
-                V.append((i, j))
+                V[(i, j)] = (i, j)
                 if i + 1 < size:
-                    E.append([abs(values[i][j] - values[i+1][j]), (i, j), (i+1, j)])
+                    E.append([abs(values[i][j] - values[i + 1][j]), (i, j), (i + 1, j)])
                 if j + 1 < size:
-                    E.append([abs(values[i][j] - values[i][j+1]), (i, j), (i, j+1)])
-                if i - 1 >= 0:
+                    E.append([abs(values[i][j] - values[i][j + 1]), (i, j), (i, j + 1)])
+                if j - 1 > -1:
+                    E.append([abs(values[i][j] - values[i][j - 1]), (i, j), (i, j - 1)])
+                if i - 1 > -1:
                     E.append([abs(values[i][j] - values[i-1][j]), (i, j), (i-1, j)])
-                if j - 1 >= 0:
-                    E.append([abs(values[i][j] - values[i][j-1]), (i, j), (i, j-1)])
         return V, E
 
     def find(self, x):
@@ -35,60 +34,45 @@ class Graph:
         return self.find(self.parent[x])
 
     def union(self, u_root, v_root, w):
-        if self.rank[u_root] < self.rank[v_root]:
-            self.parent[u_root] = v_root
-            self.max_values[u_root] = w
-        elif self.rank[u_root] > self.rank[v_root]:
-            self.parent[v_root] = u_root
-            self.max_values[v_root] = w
-        else:
-            self.parent[v_root] = u_root
-            self.max_values[v_root] = w
-            self.rank[u_root] += 1
+        self.parent[v_root] = u_root
+        self.max_values[u_root] = w
 
     def kruskal(self):
         sorted_edges = sorted(self.E, key=lambda x: x[0])
         for weight, u, v in sorted_edges:
             u_root = self.find(u)
             v_root = self.find(v)
-            if u_root != v_root and weight <= self.max_values[u_root] + 2 and weight <= self.max_values[v_root] + 2:
+            treshold = min(self.max_values[u_root], self.max_values[v_root])+2
+            if u_root != v_root and weight < treshold:
                 self.union(u_root, v_root, weight)
         return self.get_result()
 
     def get_result(self):
-
         root_diff = self.parent
         edges = []
         for u, v in root_diff.items():
-            edges.append((u[0]*self.size+u[1], v[0]*self.size+v[1]))
-            edges.append((v[0] * self.size + v[1], u[0] * self.size + u[1]))
+            edges.append((u[0] * self.size + u[1], v[0] * self.size + v[1]))
 
-        to_see = set()
-        to_see.add(0)
         background = set()
         background.add(0)
-        while len(to_see) != 0:
-            u = to_see.pop()
-            for x,y in edges:
-                if x == u:
-                    to_see.add(y)
-                    background.add(y)
-                    edges.remove((x, y))
-                elif y == u:
-                    to_see.add(x)
-                    background.add(x)
-                    edges.remove((x, y))
+        old_b = 0
+        while old_b != len(background):
+            old_b = len(background)
+            for tups in edges:
+                if not background.isdisjoint(tups):
+                    background.add(tups[0])
+                    background.add(tups[1])
 
         result = np.array([[1] * self.size] * self.size)
-        for i in range(self.size):
+        for k in range(self.size):
             for j in range(self.size):
-                if i*self.size+j in background:
-                    result[i][j] = 0
+                if k * self.size + j in background:
+                    result[k][j] = 0
         return result
 
 
-def read_file():
-    with open("test_img.txt", "r", encoding="UTF8") as f:
+def read_file(path):
+    with open(path, "r", encoding="UTF8") as f:
         n = int(f.readline())
         values = [[0] * n] * n
         for i, line in enumerate(f.readlines()):
@@ -96,8 +80,40 @@ def read_file():
     return np.array(values)
 
 
+def parse_input(input_lines, size):
+    values = [[0] * size] * size
+    for i, line in enumerate(input_lines):
+        row = list(map(int, line.split()))
+        values[i] = row
+    return np.array(values)
+
+
+def write_to_file(img):
+    with open("result.txt", "w") as f:
+        for row in img:
+            for value in row:
+                f.write(str(value) + " ")
+            f.write("\n")
+
+
+def show_out(res):
+    for row in res:
+        for value in row:
+            print(value, end=" ")
+        print("")
+
+
 if __name__ == "__main__":
-    img = read_file()
-    graph = Graph(img, img.shape[0])
-    result = graph.kruskal()
-    print(result)
+    input = [['input1.txt', 'ref1.txt'], ['input3.txt', 'ref3.txt'], ['input4.txt', 'ref4.txt'],
+             ['input5.txt', 'ref5.txt']]
+    inputs = [['input2.txt', 'ref2.txt']]
+    for file, ref in inputs:
+        print(file)
+        img = read_file(file)
+        ref = read_file(ref)
+        graph = Graph(img, img.shape[0])
+        result = np.array(graph.kruskal())
+        equal = np.array_equal(result, ref)
+        write_to_file(result)
+        print(equal)
+        print("")
